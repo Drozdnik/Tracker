f292643ee5b0d659166efdffc6fd882a7af2e2ac
Drozdnik <drozdnik@list.ru>
2024-04-07 01:14:39 +0300

    TrackerView done with custom NavController

--- Tracker/TrackerViewController.swift
+++ Tracker/TrackerViewController.swift
@@ -2,19 +2,46 @@ import Foundation
 import UIKit
 
 final class TrackerViewController: UIViewController{
+    let noTracksLabel = UILabel()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         configureNavigationBar()
+        addSubViews()
+        configureConstraints()
     }
     
     private func addSubViews(){
-        
+        view.addSubview(noTracksLabel)
+        view.addSubview(imageForNoTracks)
     }
     
     private func configureConstraints(){
-        
+        configureNoTracksLabel()
+        NSLayoutConstraint.activate([
+            imageForNoTracks.heightAnchor.constraint(equalToConstant: 80),
+            imageForNoTracks.widthAnchor.constraint(equalToConstant: 80),
+            imageForNoTracks.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            imageForNoTracks.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            
+            noTracksLabel.topAnchor.constraint(equalTo: imageForNoTracks.bottomAnchor, constant: 8),
+            noTracksLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            noTracksLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
     }
+    
+    private func configureNoTracksLabel(){
+        noTracksLabel.text = "Что будем отслеживать?"
+        noTracksLabel.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        noTracksLabel.textAlignment = .center
+        noTracksLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+    private lazy var imageForNoTracks: UIImageView = {
+        let image = UIImage(named: "NoTracks")
+        let noTracksImage = UIImageView(image: image)
+        noTracksImage.translatesAutoresizingMaskIntoConstraints = false
+        return noTracksImage
+    }()
     
     private func configureNavigationBar(){
         let customNavBar = NavigationBarCustom()
