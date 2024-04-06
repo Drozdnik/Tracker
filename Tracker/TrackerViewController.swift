7e07ba1e8fd9e52d5143e7d93cf2de66034d8af8
Drozdnik <drozdnik@list.ru>
2024-04-06 20:43:03 +0300

    TabBar is ready

---
+++ Tracker/TrackerViewController.swift
@@ -0,0 +1,24 @@
+import Foundation
+import UIKit
+
+final class TrackerViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+    private func addSubViews(){
+       
+    }
+    
+    private func configureConstraints(){
+        
+    }
+    
+//    func createTrackerViewController() -> UINavigationController {
+//        let trackerViewController = TrackerViewController()
+//        trackerViewController.title = "Трекеры"
+//        trackerViewController.tabBarItem = UITabBarItem
+//    }
+
+}
---
+++ Tracker/tabBarViewController.swift
@@ -0,0 +1,36 @@
+import Foundation
+import UIKit
+
+final class TabBarViewController: UITabBarController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViewController()
+    }
+    
+    private func setupViewController(){
+        let tracker = TrackerViewController()
+        let statistic = StatisticViewController()
+        
+        
+        let trackerViewController = UINavigationController(rootViewController: tracker)
+        let statisticViewController = UINavigationController(rootViewController: statistic)
+        trackerViewController.tabBarItem = UITabBarItem(
+            title: "Трекеры",
+            image: UIImage(named: "trackerTabBar"),
+            selectedImage: UIImage(systemName: "trackerTabBar"))
+        statisticViewController.tabBarItem = UITabBarItem(
+            title: "Статистика",
+            image: UIImage(named: "trackerTabBar"),
+            selectedImage: UIImage(named: "trackerTabBar")
+        )
+    
+        viewControllers = [trackerViewController, statisticViewController]
+        
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        tabBarAppearance.backgroundColor = .white
+        UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+        UITabBar.appearance().standardAppearance = tabBarAppearance
+        
+    }
+}
